b267900f0c72e6422445b7f55643fc5618d3c837
Jessica Kwok <JessicaKwok@Jessica-Kwoks-MacBook-Pro-2.local>
2015-03-14 18:44:28 -0700

    testing again

--- js/navbar.js
+++ js/navbar.js
@@ -1,33 +1,11 @@
-/* affix the navbar after scroll below header */
 
-// $('#nav').affix({
-//       offset: {
-//         top: $('header').height()-$('#nav').height()
-//       }
-// });	
+var a = $("#section2").offset();
 
-// /* highlight the top nav as scrolling occurs */
-// $('body').scrollspy({ target: '#nav' })
+$(window).scroll(function(){
+    if($(window).scrollTop() + $('#navbar').height() > a.top) {   
 
-// /* smooth scrolling for scroll to top */
-// $('.scroll-top').click(function(){
-//   $('body,html').animate({scrollTop:0},1000);
-// })
-
-// /* smooth scrolling for nav sections */
-// $('#nav .navbar-nav li>a').click(function(){
-//   var link = $(this).attr('href');
-//   var posi = $(link).offset().top;
-//   $('body,html').animate({scrollTop:posi},700);
-// });
-
-var a = $("fixed-nav-bar").offset().top;
-
-$(document).scroll(function(){
-    if($(this).scrollTop() > a)
-    {   
-       $('fixed-nav-bar').css({"background":"blue"});
+    	$('#navbar').css('background', 'blue');
     } else {
-       $('fixed-nav-bar').css({"background":"transparent"});
+    	$('#navbar').css('background', 'green');
     }
 });
